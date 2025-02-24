ec81a251b5c7aa74f5bc8f95d25957a64a0319b6
Rudy Guerra Jr <119625439+RudyGuerra@users.noreply.github.com>
2025-02-24 00:46:38 +0000

    dashboard page

---
+++ better-picks/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+//Used link to help with structure of code. https://chatgpt.com/c/6794242c-fa60-8012-a39d-fbb1501faf5b
+// app/dashboard/page.tsx
+const Dashboard = () => {
+    return (
+      <div>
+        <h1>Welcome to the Dashboard</h1>
+        <p>This is the content of your dashboard.</p>
+      </div>
+    );
+  };
+  
+  export default Dashboard;
+  
