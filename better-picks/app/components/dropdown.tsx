d4af87adf0759c5cf26fb2e60236255a061f8704
Rudy Guerra Jr <119625439+RudyGuerra@users.noreply.github.com>
2025-03-02 22:17:16 +0000

    Developed pages and added team logos

---
+++ better-picks/app/components/dropdown.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React from "react";
+
+interface DropdownProps {
+  title: string;
+  options: string[];
+  links: string[];
+  isOpen: boolean;
+  setOpenDropdown: React.Dispatch<React.SetStateAction<string | null>>;
+}
+
+export default function Dropdown({ title, options, links, isOpen, setOpenDropdown }: DropdownProps) {
+  return (
+    <div className="relative">
+      <button
+        onClick={() => setOpenDropdown(isOpen ? null : title)}
+        className="text-white hover:text-green-400 transition"
+      >
+        {title}
+      </button>
+      {isOpen && (
+        <ul className="absolute mt-2 w-48 bg-gray-900 rounded shadow-lg z-10">
+          {options.map((option, index) => (
+            <li key={index}>
+              <a href={links[index]} className="block px-4 py-2 hover:bg-gray-700">
+                {option}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+
