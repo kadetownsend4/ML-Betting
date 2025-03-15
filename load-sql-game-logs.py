b460d9795fb2f8d2910631823c2e12a4f5681e7a
kadetownsend4 <kadetownsend4@gmail.com>
2025-03-15 17:46:58 -0500

    was able to make sql connection

--- load-sql-game-logs.py
+++ load-sql-game-logs.py
@@ -1,2 +1,10 @@
-import sys
-print(sys.version)
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Baseball2003!",
+    database="BetterPicks"
+)
+
+cursor = connection.cursor()
