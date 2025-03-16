16ac190aebe95afa9e04754aa0e1dff5f9d75e30
kadetownsend4 <kadetownsend4@gmail.com>
2025-03-16 14:24:28 -0500

    working on try to get gamelogs to mysql database

--- load-sql-game-logs.py
+++ load-sql-game-logs.py
@@ -1,4 +1,12 @@
 import mysql.connector
+import pandas as pd
+import csv
+
+game_logs = pd.read_csv("GettingData/gameLogs.csv")
+
+#print(game_logs)
+
+csv_filepath = "GettingData/gameLogs.csv"
 
 connection = mysql.connector.connect(
     host="localhost",
@@ -8,3 +16,22 @@ connection = mysql.connector.connect(
 )
 
 cursor = connection.cursor()
+
+cursor.execute("DROP TABLE nbagamelogs")
+
+with open(csv_filepath, 'r') as file:
+    csv_reader = csv.reader(file)
+    header = next(csv_reader)
+
+    create_table_query = f"CREATE TABLE IF NOT EXISTS nbagamelogs ({', '.join([f'{col} VARCHAR(255)' for col in header])})"
+    cursor.execute(create_table_query)
+
+    insert_query = f"INSERT INTO your_table ({', '.join(header)}) VALUES ({', '.join(['%s'] * len(header))})"
+
+    for row in csv_reader:
+        cursor.execute(insert_query, row)
+
+    connection.commit()
+
+connection.close()
+cursor.close()
