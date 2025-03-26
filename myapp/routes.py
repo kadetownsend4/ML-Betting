10ac681ac75ee806136e76c763b75e2189b0ca38
TimB21 <122762696+TimB21@users.noreply.github.com>
2025-03-26 18:51:36 -0500

    Created user model along with templates to testing adding users and login

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,19 +1,50 @@
-from flask import Blueprint, redirect, url_for 
-
-from .extensions import db 
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from .models import User
+from .extensions import db 
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
     users = User.query.all()
-    users_list_html = [f"<li>{ user.username }</li>" for user in users]
+    users_list_html = [f"<li>{user.username}</li>" for user in users]
     return f"<ul>{''.join(users_list_html)}</ul>"
 
-@main.route('/add/<username>')
-def add_user(username):
-    db.session.add(User(username=username))
-    db.session.commit()
-    return redirect(url_for("main.index"))
+# I referenced chatgpt for help creating methods for adding users and login.
+# https://chatgpt.com/share/67e49261-fcfc-800f-a302-03d2a6125d48
+@main.route('/add', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        
+        # Create new user with the provided info
+        new_user = User(username=username, email=email)
+        new_user.set_password(password)  # Hash the password
+        db.session.add(new_user)
+        db.session.commit()
+        
+        return redirect(url_for('main.index'))
 
+    return render_template('register.html')
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Retrieve form data
+        username = request.form['username']
+        password = request.form['password']
+        
+        # Find the user by username
+        user = User.query.filter_by(username=username).first()
+        
+        if user and user.check_password(password):  # Check if the password matches
+            # User is authenticated, redirect to a protected page
+            flash("Login successful!", category="success")
+            return redirect(url_for('main.index'))  # Or any other page you want to redirect to
+        else:
+            flash("Invalid username or password", category="error")
+    
+    # If GET request, just render the login page
+    return render_template('login.html')
