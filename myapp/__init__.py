60fb00963f4c87e34efd918679a948e13f079fba
TimB21 <122762696+TimB21@users.noreply.github.com>
2025-03-26 18:51:36 -0500

    Created user model along with templates to testing adding users and login

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -2,17 +2,22 @@ import os
 
 from flask import Flask
 from flask_cors import CORS
+from flask_migrate import Migrate
 
 def create_app():
     from .extensions import db
     from .routes import main
 
     app = Flask(__name__)
-    CORS(app)
+    
+    # Initialize Migrate
+    migrate = Migrate(app, db)
 
+    CORS(app)
+    # Set the SECRET_KEY from environment variable or fallback to a default one if not set
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
     db.init_app(app)
-
     app.register_blueprint(main)
 
     return app
