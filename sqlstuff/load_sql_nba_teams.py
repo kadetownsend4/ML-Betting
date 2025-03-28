e80ce07fa7a6027e7baae8bbc1e63e605a0d8305
kadetownsend4 <kadetownsend4@gmail.com>
2025-03-28 12:16:49 -0500

    working on connecting db

--- sqlstuff/load_sql_nba_teams.py
+++ sqlstuff/load_sql_nba_teams.py
@@ -5,12 +5,13 @@ from nba_api.stats.static import teams
 
 nba_teams = teams.get_teams()
 nba_teams_df = pd.DataFrame.from_dict(nba_teams)
-nba_teams_df.rename(columns={'id': 'TEAM_ID', 'full_name': 'TEAM_NAME', 'abbreviation': 'TEAM_ABR', 'nickname': 'TEAM_NICKNAME', 'city': 'TEAM_CITY', 'state': 'TEAM_STATE', 'year_founded': 'TEAM_YEAR_FOUNDED'}, inplace=True)
+nba_teams_df.rename(columns={'id': 'TEAM_ID', 'full_name': 'TEAM_NAME', 'abbreviation': 'TEAM_ABR', 'nickname': 'TEAM_NICKNAME',
+                    'city': 'TEAM_CITY', 'state': 'TEAM_STATE', 'year_founded': 'TEAM_YEAR_FOUNDED'}, inplace=True)
 nba_teams_df.at[7, 'TEAM_CITY'] = "San Francisco"
 nba_teams_df.at[13, 'TEAM_CITY'] = "Minneapolis"
 nba_teams_df.at[15, 'TEAM_CITY'] = "New York City"
 nba_teams_df.at[17, 'TEAM_CITY'] = "Indianapolis"
 nba_teams_df.at[25, 'TEAM_CITY'] = "Salt Lake City"
-#print(nba_teams_df)
+# print(nba_teams_df)
 
-sqlalchemy_interact.insert_df_to_mysql_sqlalchemy(nba_teams_df, "teams")
+sqlalchemy_interact.insert_df_to_mysql_sqlalchemy(nba_teams_df, "NBAteams")
--- sqlstuff/sqlalchemy_interact.py
+++ sqlstuff/sqlalchemy_interact.py
@@ -1,17 +1,24 @@
 import pandas as pd
 from sqlalchemy import create_engine
+import os
+
+user = os.environ.get('DB_USER')
+pw = os.environ.get('DB_PW')
+host = os.environ.get('DB_HOST')
+port = 5432
+name = os.environ.get('DB_NAME')
 
 
 def insert_df_to_mysql_sqlalchemy(df, table_name):
     engine = create_engine(
-        f"mysql+mysqlconnector://{'root'}:{'Baseball2003!'}@{'localhost'}:{'3306'}/{'BetterPicks'}")
+        f"postgresql://{user}:{pw}@{host}:{port}/{name}")
     df.to_sql(name=table_name, con=engine, if_exists='replace', index=False)
-    #print(f"DataFrame successfully inserted into table '{table_name}'")
+    print(f"DataFrame successfully inserted into table '{table_name}'")
 
 
 def get_df_from_mysql_sqlalchemy(sql):
     engine = create_engine(
-        f"mysql+mysqlconnector://{'root'}:{'Baseball2003!'}@{'localhost'}:{'3306'}/{'BetterPicks'}")
+        f"postgresql://{user}:{pw}@{host}:{port}/{name}")
     df = pd.read_sql(sql, engine)
     #print("DataFrame successfully gathered")
     return df
