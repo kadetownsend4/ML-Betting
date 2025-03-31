9a5b5ed5d34c8248f76623df4674434b6ebb89cf
kadetownsend4 <kadetownsend4@gmail.com>
2025-03-30 20:58:16 -0500

    added nbateams table and nbagamelogs (only name)

--- sqlstuff/load-sql-game-logs.py
+++ sqlstuff/load-sql-game-logs.py
@@ -10,5 +10,7 @@ import sqlalchemy_interact
 # puts csv file of game logs into df
 df = pd.read_csv("GettingData/gameLogs.csv")
 
+df = df.drop("Unnamed: 0", axis=1)
+
 # inserts df to database under name "nbagamelogs"
 sqlalchemy_interact.insert_df_to_mysql_sqlalchemy(df, "nbagamelogs")
