4a9db3741c76bb8033a68b843b3f8b84711bfaa2
kkeleher77 <149116105+kkeleher77@users.noreply.github.com>
2025-04-07 17:12:52 +0000

    Made some changes to dashboard and was making tweaks to the team stats and latest games pages

--- better-picks/app/components/Dashboard.tsx
+++ better-picks/app/components/Dashboard.tsx
@@ -1,11 +1,11 @@
 "use client";
-import { useState } from "react";
+import { useState, ReactNode } from "react";
 import Link from "next/link";
 import { motion, AnimatePresence } from "framer-motion";
 
 type DashboardProps = {
   children: ReactNode;
-}
+};
 
 const menuItems = [
   {
@@ -42,61 +42,67 @@ const menuItems = [
     ],
   },
 ];
+
 const Dashboard: React.FC<DashboardProps> = ({ children }) => {
   const [activeDropdown, setActiveDropdown] = useState<string | null>(null);
+
   return (
     <div className="relative z-50">
-      <header className="z-50 flex justify-between items-center w-full py-4 px-8 bg-gray-800 backdrop-blur-md rounded-lg shadow-lg border border-white/15">
-  <Link href="/" className="text-4xl font-extrabold text-green shadow-none border-b-4 border-green-400 hover:text-green-300 transition">
-    Better Picks
-  </Link>
-        <nav className="flex space-x-10 relative">
-          {menuItems.map((item, index) => {
-            const isActive = activeDropdown === item.title;
-            return (
-              <div
-                key={index}
-                className="relative flex flex-col items-center"
-                onMouseEnter={() => setActiveDropdown(item.title)}
-                onMouseLeave={() => setActiveDropdown(null)}
-              >
-                <Link href={item.path || "#"}>
+      <header className="z-50 flex justify-center w-full py-2 px-4">
+        <div className="flex justify-between items-center w-full max-w-8xl px-8 bg-gray-800 backdrop-blur-md rounded-lg shadow-lg border border-white/15">
+          <Link
+            href="/"
+            className="text-4xl font-extrabold text-green shadow-none border-b-4 border-green-400 hover:text-green-300 transition"
+          >
+            Better Picks
+          </Link>
+          <nav className="flex space-x-10 relative">
+            {menuItems.map((item, index) => {
+              const isActive = activeDropdown === item.title;
+              return (
+                <div
+                  key={index}
+                  className="relative flex flex-col items-center"
+                  onMouseEnter={() => setActiveDropdown(item.title)}
+                  onMouseLeave={() => setActiveDropdown(null)}
+                >
                   <button className="text-xl font-bold hover:text-green-400 transition">
                     {item.title}
                   </button>
-                </Link>
-                {!item.noDropdown && (
-                  <AnimatePresence>
-                    {isActive && (
-                      <motion.div
-                        key="dropdown"
-                        initial={{ opacity: 0, y: 10, scale: 0.95 }}
-                        animate={{ opacity: 1, y: 0, scale: 1 }}
-                        exit={{ opacity: 0, y: 10, scale: 0.95 }}
-                        transition={{ duration: 0.3, ease: "easeInOut" }}
-                        className="absolute top-full mt-2 w-56 bg-gray-800/95 backdrop-blur-md border border-white/20 rounded-3xl shadow-xl p-3 z-50"
-                      >
-                        {item.links?.map((link, idx) => (
-                          <Link
-                            key={idx}
-                            href={link.path}
-                            className="block px-4 py-2 hover:bg-white/20 rounded-lg transition duration-200 text-white text-sm text-center"
-                          >
-                            {link.name}
-                          </Link>
-                        ))}
-                      </motion.div>
-                    )}
-                  </AnimatePresence>
-                )}
-              </div>
-            );
-          })}
-        </nav>
+                  {!item.noDropdown && (
+                    <AnimatePresence>
+                      {isActive && (
+                        <motion.div
+                          key="dropdown"
+                          initial={{ opacity: 0, y: 10, scale: 0.95 }}
+                          animate={{ opacity: 1, y: 0, scale: 1 }}
+                          exit={{ opacity: 0, y: 10, scale: 0.95 }}
+                          transition={{ duration: 0.3, ease: "easeInOut" }}
+                          className="absolute top-full mt-2 w-56 bg-gray-800/95 backdrop-blur-md border border-white/20 rounded-3xl shadow-xl p-3 z-50"
+                        >
+                          {item.links?.map((link, idx) => (
+                            <Link
+                              key={idx}
+                              href={link.path}
+                              className="block px-4 py-2 hover:bg-white/20 rounded-lg transition duration-200 text-white text-sm text-center"
+                            >
+                              {link.name}
+                            </Link>
+                          ))}
+                        </motion.div>
+                      )}
+                    </AnimatePresence>
+                  )}
+                </div>
+              );
+            })}
+          </nav>
+        </div>
       </header>
 
-      <main className="mt-8 px-8"> { children} </main>
+      <main className="mt-8 px-8">{children}</main>
     </div>
   );
 };
+
 export default Dashboard;
