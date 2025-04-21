89f0bb9c0ddbe6d16e52c10d026d8e8183fe6969
TimB21 <122762696+TimB21@users.noreply.github.com>
2025-04-21 14:24:49 -0500

    Changed child for dashboard

--- better-picks/app/nfl-teams/page.tsx
+++ better-picks/app/nfl-teams/page.tsx
@@ -136,50 +136,9 @@ function NFLTeams() {
 
 
 
-      {/* Header Section */}
-  <header className="flex justify-between items-center w-full py-4 px-8 bg-white/10 backdrop-blur-md rounded-lg shadow-lg border border-white/15">
-  <h1 className="text-4xl font-extrabold text-white drop-shadow-2xl border-b-4 border-green-400">
-    Better Picks
-  </h1>
-  <nav className="flex space-x-10 relative">
-    {menuItems.map((item, index) => (
-      <div
-        key={index}
-        className="relative group flex flex-col items-center"
-        onMouseEnter={() => setActiveDropdown(item.title)}
-        onMouseLeave={() => setActiveDropdown(null)}
-      >
-        <button className="text-xl font-bold hover:text-green-400 transition">
-          {item.title}
-        </button>
-
-        <AnimatePresence>
-          {activeDropdown === item.title && (
-            <motion.div
-              initial={{ opacity: 0, y: 10, scale: 0.95 }}
-              animate={{ opacity: 1, y: 0, scale: 0.80 }}
-              exit={{ opacity: 0, y: 10, scale: 0.95 }}
-              transition={{ duration: 0.3, ease: "easeInOut" }}
-              className="absolute left-1/8 mt-2 w-40 bg-white/5 backdrop-blur-md border border-white/10 rounded-3xl shadow-xl p-3 z-50"
-              onMouseEnter={() => setActiveDropdown(item.title)}
-              onMouseLeave={() => setActiveDropdown(null)}
-            >
-              {item.links.map((link, idx) => (
-                <Link
-                  key={idx}
-                  href={link.path}
-                  className="block px-4 py-2 bg-transparent hover:bg-white/20 rounded-lg transition-all duration-200 ease-in-out text-center w-full"
-                >
-                  {link.name}
-                </Link>
-              ))}
-            </motion.div>
-          )}
-        </AnimatePresence>
-      </div>
-    ))}
-  </nav>
-</header>
+     <Dashboard>    
+        
+     </Dashboard>
 
 
       {/* Teams Section */}
